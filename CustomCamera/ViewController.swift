0d4647b27fcd6f29f0fd575eba7f55414f3ef54c
Ronald Arias <ronald.arias@udla.edu.ec>
2019-11-05 22:11:47 -0500

    Proyecto Final

--- CustomCamera/ViewController.swift
+++ CustomCamera/ViewController.swift
@@ -7,22 +7,20 @@
 //
 
 import UIKit
-
-//1. importamos AVFoundation
 import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     
-    //2. creamos las variables y constantes para usar en la camara
     let captureSession = AVCaptureSession()
     var previewLayer: CALayer!
     var captureDevice: AVCaptureDevice!
     
+    //1. creamos la variable booleana takePhoto para saber si tomo la foto
+    var takePhoto = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        //3. llamamos a la funcion prepareCamera()
         prepareCamera()
     }
     
@@ -34,7 +32,6 @@ class ViewController: UIViewController {
         let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
         captureDevice = availableDevices.first
         
-        //4. llamamos a la funcion beginSession()
         beginSession()
     }
     
@@ -70,7 +67,65 @@ class ViewController: UIViewController {
         
         
         let queue = DispatchQueue(label: "com.brianadvent.captureQueue")
-        dataOutput.setSampleBufferDelegate((self as! AVCaptureVideoDataOutputSampleBufferDelegate), queue: queue)
+        dataOutput.setSampleBufferDelegate(self, queue: queue)
     }
+    
+    //2. escribimos el codigo que se ejecutara al momento de presionar el boton de la camara
+    @IBAction func takePhotoPressed(_ sender: UIBarButtonItem) {
+        takePhoto = true
+    }
+    
+    //3. invocamos un metodo del delegate llamado captureOutput
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        if takePhoto {
+            takePhoto = false
+            
+            //4. llamamos al metodo getImageFromSampleBuffer para obtener la imagen
+            if let image = self.getImageFromSampleBuffer(buffer: sampleBuffer) {
+                
+                let photoVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "PhotoVC") as! PhotoViewController
+                
+                photoVC.takenPhoto = image
+                
+                DispatchQueue.main.async {
+                    self.present(photoVC, animated: true, completion: {
+                        //5. detenemos la captura
+                        self.stopCaptureSession()
+                    })
+                    
+                }
+            }
+        }
+    }
+    
+    func getImageFromSampleBuffer (buffer:CMSampleBuffer) -> UIImage? {
+        
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(buffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let context = CIContext()
+            
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image, scale: UIScreen.main.scale, orientation: .right)
+            }
+            
+        }
+        
+        return nil
+    }
+    
+    func stopCaptureSession () {
+        self.captureSession.stopRunning()
+        
+        if let inputs = captureSession.inputs as? [AVCaptureDeviceInput] {
+            for input in inputs {
+                self.captureSession.removeInput(input)
+            }
+        }
+        
+    }
+    
 }
 

6f692bb9259ccb1796041c88dae2b5422506021d
Ronald Arias <ronald.arias@udla.edu.ec>
2019-11-05 20:10:05 -0500

    Modificando ViewController

--- CustomCamera/ViewController.swift
+++ CustomCamera/ViewController.swift
@@ -8,13 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//1. importamos AVFoundation
+import AVFoundation
 
+class ViewController: UIViewController {
+    
+    //2. creamos las variables y constantes para usar en la camara
+    let captureSession = AVCaptureSession()
+    var previewLayer: CALayer!
+    var captureDevice: AVCaptureDevice!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //3. llamamos a la funcion prepareCamera()
+        prepareCamera()
     }
-
-
+    
+    
+    func prepareCamera() {
+        
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        captureDevice = availableDevices.first
+        
+        //4. llamamos a la funcion beginSession()
+        beginSession()
+    }
+    
+    func beginSession() {
+        
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession.addInput(captureDeviceInput)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer = previewLayer
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.layer.frame
+        captureSession.startRunning()
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.videoSettings = [((kCVPixelBufferPixelFormatTypeKey as NSString) as String):NSNumber(value:kCVPixelFormatType_32BGRA)]
+        
+        dataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        if captureSession.canAddOutput(dataOutput) {
+            captureSession.addOutput(dataOutput)
+        }
+        
+        captureSession.commitConfiguration()
+        
+        
+        let queue = DispatchQueue(label: "com.brianadvent.captureQueue")
+        dataOutput.setSampleBufferDelegate((self as! AVCaptureVideoDataOutputSampleBufferDelegate), queue: queue)
+    }
 }
 
